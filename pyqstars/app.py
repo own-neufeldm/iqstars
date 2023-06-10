553566c6365475fd0eefdac067866a5f2a853398
Martin Neufeld <martin.neufeld@oediv.de>
2023-06-10 23:35:50 +0200

    stash

--- pyqstars/app.py
+++ pyqstars/app.py
@@ -1,3 +1,37 @@
+import textwrap
+
+
+def solve(board: list[list[str]]) -> list[list[str]]:
+    return board
+
+
+def dumps(board: list[list[str]]) -> str:
+    lines: list[str] = []
+    for row in board:
+        line = []
+        for char in row:
+            line.append(char.upper())
+        lines.append(" ".join(line))
+    return "\n".join(lines)
+
+
 def main() -> None:
-    print("'Hello, World!' ~ pyqstars")
+    board = textwrap.dedent("""\
+        v v v - - - -
+         - - - - - -
+        - - - - - - -
+         - - - - - -\
+    """)
+    board = [row.split(" ") for row in board.split("\n")]
+    solution = solve(board)
+    print(
+        "",
+        "Attempting to solve:\n",
+        dumps(board),
+        "\n# ---------------------------------------------- #\n",
+        "Solution:\n",
+        dumps(solution),
+        "",
+        sep="\n"
+    )
     return None
