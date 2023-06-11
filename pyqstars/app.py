a91d2b27cd745975da51a626bfe2b9a97210127b
Neufeld-Martin <m.neufeld946@gmail.com>
2023-06-11 18:01:57 +0200

    feat: Board and Piece

--- pyqstars/app.py
+++ pyqstars/app.py
@@ -3,57 +3,95 @@ import textwrap
 from pyqstars.core.objects import Board, Piece
 
 
-PIECES = [
-    Piece("b", "blue"),
-    Piece("g", "green"),
-    Piece("o", "orange"),
-    Piece("p", "pink"),
-    Piece("r", "red"),
-    Piece("v", "violet"),
-    Piece("y", "yellow")
-]
-
-
-def place(board: list[list[str]], piece: str) -> bool:
-    return True
-
-
-def solve(board: Board, pieces: list[Piece]) -> Board | None:
-    if not pieces:
-        return board
-    for piece in pieces:
-        if Board.place(piece):
-            Board.solve(board, pieces)
-    return None
-
-
-def get_unused_pieces(board: list[list[str]]) -> list[str]:
-    unused_pieces: list[str] = []
-    for piece in PIECES:
-        for rotattion in range(6):
-            rotated_piece: str = piece  # ... rotate somehow
-            if not any(rotated_piece in row for row in board):
-                unused_pieces.append(rotated_piece)
-    return unused_pieces
-
-
 def main() -> None:
+    blue = Piece.from_string("b", "blue", textwrap.dedent("""\
+        * * *
+           *"""))
+    green = Piece.from_string("g", "green", textwrap.dedent("""\
+        * *
+           *
+            *"""))
+    orange = Piece.from_string("o", "orange", textwrap.dedent("""\
+        * *
+           *"""))
+    pink = Piece.from_string("p", "pink", textwrap.dedent("""\
+        * *
+           *
+          *"""))
+    red = Piece.from_string("r", "red", textwrap.dedent("""\
+        * *
+         * *"""))
+    violet = Piece.from_string("v", "violet", textwrap.dedent("""\
+        * * *"""))
+    yellow = Piece.from_string("y", "yellow", textwrap.dedent("""\
+        * * *
+         *"""))
     board = Board.from_string(textwrap.dedent("""\
-        v v v - - - -
-         - - - - - -
-        - - - - - - -
-         - - - - - -\
-    """))
-    print(
-        f"\nAttempting to solve:\n\n"
-        f"{board!s}\n\n"
-        f"# ---------------------------------------------- #\n",
-    )
-    if not Board.solve(PIECES.values()):
-        print("Board cannot be solved!\n")
-    else:
-        print(
-            f"Solution:\n\n"
-            f"{board!s}\n"
-        )
+        v v v r o y y
+         p p r r o y
+        p b p r o y g
+         b b b g g g"""))
+    board.inspect()
+    print("\n# ---------------------------------------------- #\n")
+    board.dump()
+    print("\n# ---------------------------------------------- #\n")
+    pink.inspect()
+    print("\n# ---------------------------------------------- #\n")
+    pink.dump()
     return None
+
+
+# PIECES = [
+#     Piece("b", "blue"),
+#     Piece("g", "green"),
+#     Piece("o", "orange"),
+#     Piece("p", "pink"),
+#     Piece("r", "red"),
+#     Piece("v", "violet"),
+#     Piece("y", "yellow")
+# ]
+
+
+# def place(board: list[list[str]], piece: str) -> bool:
+#     return True
+
+
+# def solve(board: Board, pieces: list[Piece]) -> Board | None:
+#     if not pieces:
+#         return board
+#     for piece in pieces:
+#         if Board.place(piece):
+#             Board.solve(board, pieces)
+#     return None
+
+
+# def get_unused_pieces(board: list[list[str]]) -> list[str]:
+#     unused_pieces: list[str] = []
+#     for piece in PIECES:
+#         for rotattion in range(6):
+#             rotated_piece: str = piece  # ... rotate somehow
+#             if not any(rotated_piece in row for row in board):
+#                 unused_pieces.append(rotated_piece)
+#     return unused_pieces
+
+
+# def main() -> None:
+#     board = Board.from_string(textwrap.dedent("""\
+#         v v v - - - -
+#          - - - - - -
+#         - - - - - - -
+#          - - - - - -\
+#     """))
+#     print(
+#         f"\nAttempting to solve:\n\n"
+#         f"{board!s}\n\n"
+#         f"# ---------------------------------------------- #\n",
+#     )
+#     if not Board.solve(PIECES.values()):
+#         print("Board cannot be solved!\n")
+#     else:
+#         print(
+#             f"Solution:\n\n"
+#             f"{board!s}\n"
+#         )
+#     return None
--- pyqstars/core/objects.py
+++ pyqstars/core/objects.py
@@ -1,34 +1,120 @@
 from dataclasses import dataclass
+from pprint import pprint
 from typing import Self
 
 
 @dataclass(frozen=True)
 class Piece:
+    CHAR_TO_TILE = {
+        " ": False,
+        "*": True
+    }
     id: str
     color: str
+    shape: list[list[bool]]
+
+    @classmethod
+    def from_string(cls, id: str, color: str, string: str) -> Self:
+        conversion = [list(row) for row in string.split("\n")]
+        shape: list[list[bool]] = []
+        rows, cols = 7, 7
+        for i in range(rows):
+            row: list[bool] = []
+            for j in range(cols):
+                try:
+                    char = conversion[i][j]
+                except IndexError:
+                    row.append(False)
+                else:
+                    row.append(cls.CHAR_TO_TILE[char])
+            shape.append(row)
+        return cls(id, color, shape)
+
+    def dumps(self) -> str:
+        tile_to_char = {v: k for k, v in type(self).CHAR_TO_TILE.items()}
+        lines: list[str] = []
+        for row in self.shape:
+            line = [tile_to_char[tile] for tile in row]
+            if string := "".join(line).rstrip():
+                lines.append(string)
+        return "\n".join(lines)
+
+    def dump(self) -> None:
+        return print(self.dumps())
+
+    def inspect(self) -> str:
+        pprint(self.shape)
+
+    def __repr__(self) -> str:
+        return self.dumps()
+
+    def __str__(self) -> str:
+        return self.inspect()
 
 
 @dataclass(frozen=True)
 class Board:
+    FIELD_EMPTY = " "
+    FIELD_UNAVAILABLE = "/"
+    CHAR_TO_FIELD = {
+        "-": FIELD_EMPTY,
+        " ": FIELD_UNAVAILABLE
+    }
     board: list[list[str]]
 
     @classmethod
     def from_string(cls, string: str) -> Self:
-        return cls([row.split(" ") for row in string.split("\n")])
+        conversion = [list(row) for row in string.split("\n")]
+        board: list[list[str]] = []
+        rows, cols = 7, 13
+        for i in range(rows):
+            if i % 2 == 1:
+                board.append(list(cls.FIELD_UNAVAILABLE for _ in range(cols)))
+                continue
+            row: list[str] = []
+            for j in range(cols):
+                try:
+                    char = conversion[i//2][j]
+                except IndexError:
+                    row.append(cls.FIELD_UNAVAILABLE)
+                else:
+                    try:
+                        row.append(cls.CHAR_TO_FIELD[char])
+                    except KeyError:
+                        row.append(char)
+            board.append(row)
+        return cls(board)
 
-    def solve(self, pieces: list[Piece]) -> bool:
-        if not pieces:
-            return True
-        for piece in pieces:
-            if Board.place(piece):
-                Board.solve(board, pieces)
-        return False
-
-    def __str__(self) -> str:
+    def dumps(self) -> str:
+        field_to_char = {v: k for k, v in type(self).CHAR_TO_FIELD.items()}
         lines: list[str] = []
         for row in self.board:
             line: list[str] = []
-            for char in row:
-                line.append(char)
-            lines.append(" ".join(line))
+            for field in row:
+                try:
+                    line.append(field_to_char[field])
+                except KeyError:
+                    line.append(field)
+            if string := "".join(line).rstrip():
+                lines.append(string)
         return "\n".join(lines)
+
+    def dump(self) -> None:
+        return print(self.dumps())
+
+    def inspect(self) -> str:
+        pprint(self.board)
+
+    def __repr__(self) -> str:
+        return self.dumps()
+
+    def __str__(self) -> str:
+        return self.inspect()
+
+    # def solve(self, pieces: list[Piece]) -> bool:
+    #     if not pieces:
+    #         return True
+    #     for piece in pieces:
+    #         if Board.place(piece):
+    #             Board.solve(board, pieces)
+    #     return False
